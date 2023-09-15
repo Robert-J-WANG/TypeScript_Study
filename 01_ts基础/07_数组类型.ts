5981dec237048cf0ca01343a479f39bbd72ccdb9
Robert-J-WANG <robin.nz420@gmail.com>
2023-09-15 15:53:24 +1200

    数组

---
+++ 01_ts基础/07_数组类型.ts
@@ -0,0 +1,23 @@
+/* ------------------------ 数组的类型 ----------------------- */
+// 在 TypeScript 中，数组类型有多种定义方式，比较灵活。
+
+/* -------------------- 类型 + 方括号」表示法 -------------------- */
+const arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+arr.push(10);
+// arr.push('11') // 类型“string”的参数不能赋给类型“number”的参数
+console.log(arr.join("-")); // 1-2-3-4-5-6-7-8-9-10
+
+// const arr2: number[] = [1, 2,3,4 ,'hello', 'world',]
+// 不能将类型“string”分配给类型“number”
+
+/* ---------------- 数组泛型 Array<elemType> ---------------- */
+const arr2: Array<number> = [1, 2, 3, 4, 5];
+console.log(arr2.map((item) => item * 2)); // [ 2, 4, 6, 8, 10 ]
+
+/* ----------------------- any 在数组中的应用 ---------------------- */
+const list: any[] = ["jack", 18, "https://www.google.com", true];
+list.forEach((li) => console.log(li));
+
+const arr3: Array<any> = [1, 2, 3, 4, "hello", "world"];
+console.log(arr3.filter((item) => typeof item === "string"));
+// [ 'hello', 'world' ]
